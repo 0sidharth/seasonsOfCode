ab2ce0834b4f030fd490d253f8a2634bbc8c15e4
Sidharth Mundhra <sidharthmundhra16@gmail.com>
2020-04-20 15:07:18 +0530

    Fixed largestrect according to contest, hogwarts finally worksgit add largestrect.cpp

--- hogwarts.cpp
+++ hogwarts.cpp
@@ -1,3 +1,4 @@
+// https://www.codechef.com/JULY17/problems/IPCTRAIN
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -6,47 +7,66 @@ using namespace std;
 #define ll long long
 
 struct teachers {
-    int day;
-    int lecture;
-    int curse;
+    ll day;
+    ll lecture;
+    ll curse;
+    ll index;
 };
 
 bool check(teachers a, teachers b) {
     return a.curse > b.curse;
 }
 
+bool checkday(teachers a, teachers b) {
+    return a.day < b.day;
+}
+
+struct checkDay {
+    bool operator()(teachers const& a, teachers const& b) {
+        return a.curse < b.curse;
+    }
+};
+
 int main() {
     int t;
     std::ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    cout.tie(NULL);
     cin >> t;
     while (t--) {
-        int n, d;
+        ll n, d;
+        ll curseTotal = 0, curseTotalSaved = 0;
         cin >> n >> d;
         teachers a[n];
-        int schedule[d];
-        int curseTotal = 0;
-        for (int i = 0; i < n; i++) {
+        for (ll i = 0; i < n; i++) {
             cin >> a[i].day >> a[i].lecture >> a[i].curse;
-        }
-        for (int i = 0; i < d; i++) {
-            schedule[i] = -1;
-        }
-        sort (a, a + n, check);
-        for (int i = 0; i < n; i++) {
-            if (a[i].lecture > d - a[i].day + 1) {
-                curseTotal += (a[i].lecture - d + a[i].day - 1) * a[i].curse;
-                a[i].lecture = d - a[i].day + 1;
-            }
-            int arrival = a[i].lecture;
-            for (int j = 0; j < arrival; j++) {
-                if (schedule[a[i].day - 1 + j] == -1) {
-                    schedule[a[i].day - 1 + j] = i + 1;
-                    a[i].lecture -= 1;
-                }
-            }
+            a[i].index = i;
             curseTotal += a[i].lecture * a[i].curse;
         }
-        cout << curseTotal << endl;
+        sort (a, a + n, checkday);
+        for (ll i = 0; i < n; i++) {
+            a[i].index = i;
+        }
+/*        for (int i = 0; i < n; i++) {
+            cout << daywise[i].index << " " << daywise[i].lecture << " " << daywise[i].day;
+        }*/
+        priority_queue < teachers, vector<teachers>, checkDay > curseDayWise;
+        ll count = 0;
+        for (int i = 1; i <= d; i++) {
+            while (count < n && a[count].day == i) {
+                curseDayWise.push(a[count]);
+                count++;
+            }
+            if (curseDayWise.empty()) continue;
+//            cerr  << a[curseDayWise.top().index].lecture << " "  << curseDayWise.top().index;
+            curseTotalSaved += curseDayWise.top().curse;
+            a[curseDayWise.top().index].lecture = a[curseDayWise.top().index].lecture - 1;
+//            cerr << " " <<  a[curseDayWise.top().index].lecture << "<<<";
+            if (a[curseDayWise.top().index].lecture == 0) {
+                curseDayWise.pop();
+            }
+//            cerr << curseTotalSaved << " " << curseDayWise.top().lecture << endl;
+        }
+        cout << curseTotal - curseTotalSaved << endl;
     }
 }
--- largestrect.cpp
+++ largestrect.cpp
@@ -1,20 +1,50 @@
-class Solution {
-public:
-    int largestRectangleArea(vector<int>& heights) {
-    stack<int> s;
-    int max = 0;
-    int start;
-    int area;
+#include <bits/stdc++.h>
 
-    int i = 0;
-    while (i < heights.size()) {
-        if (s.size() == 0 || heights[s.top()] <= heights[i]) {
-            s.push(i++);
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+
+int main() {
+    ll T = 1;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    while (1) {
+        cin >> T;
+        if (T == 0) break;
+        ll x, n = T;
+        vector<ll> heights;
+        for (ll i = 0; i < n; i++) {
+            cin >> x;
+            heights.push_back(x);
         }
-        else {
+        stack<ll> s;
+        ll start, area, max = 0, i = 0;
+        while (i < heights.size()) {
+            if (s.size() == 0 || heights[s.top()] <= heights[i]) {
+                s.push(i++);
+            }
+            else {
+                start = s.top();
+                s.pop();
+                ll width;
+                if (s.size() == 0) {
+                    width = i;
+                }
+                else {
+                    width = i - s.top() - 1;
+                }
+                area = heights[start] * width;
+                if (area > max) {
+                  max = area;
+                }
+            }
+        }
+        while (s.size() != 0) {
             start = s.top();
             s.pop();
-            int width;
+            ll width;
             if (s.size() == 0) {
                 width = i;
             }
@@ -23,27 +53,9 @@ public:
             }
             area = heights[start] * width;
             if (area > max) {
-              max = area;
+                max = area;
             }
         }
+        cout << max << endl;
     }
-
-    while (s.size() != 0) {
-        start = s.top();
-        s.pop();
-        int width;
-        if (s.size() == 0) {
-            width = i;
-        }
-        else {
-            width = i - s.top() - 1;
-        }
-        area = heights[start] * width;
-        if (area > max) {
-            max = area;
-        }
-    }
-
-    return max;
-    }
-};
+}
